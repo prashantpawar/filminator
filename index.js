d0fed50a24079e68b83d544ffb982436727fc2df
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-12-03 03:48:59 +0000

    -graphicsmagick identification of sample files

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-var im = require('imagemagick');
-im.identify('training-data/tmax-100-1.jpg', function(err, features){
-  if (err) throw err;
-  console.log(features);
-})
+var fs = require('fs'), 
+    gm = require('gm');
+var tmax100_1 = 'training-data/tmax-100-1.jpg',
+    velvia50_1 = 'training-data/velvia-50-1.jpg';
+
+gm(tmax100_1)
+.size(function (err, size) {
+  if (!err) {
+    console.log(size);
+  }
+});
+
+gm(velvia50_1)
+.identify(function (err, metadata) {
+    if(!err) {
+        console.log(metadata);
+    }
+});
