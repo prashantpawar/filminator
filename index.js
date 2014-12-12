36b83beaa6d0c4cafa984edc9011332db74415ba
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-12-12 04:48:31 +0000

    don't remember the changes

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 var fs = require('fs'), 
-    gm = require('gm');
-var tmax100_1 = 'training-data/tmax-100-1.jpg',
-    velvia50_1 = 'training-data/velvia-50-1.jpg';
-
-gm(tmax100_1)
-.size(function (err, size) {
-  if (!err) {
-    console.log(size);
-  }
-});
-
-gm(velvia50_1)
-.identify(function (err, metadata) {
-    if(!err) {
-        console.log(metadata);
-    }
+    gm = require('gm'),
+    directory = './training-data/';
+    
+fs.readdir(directory,function(err,files){
+    if(err) throw err;
+    files.forEach(function(file){
+        // do something with each file HERE!
+        gm(directory + file)
+        .size(function (err, metadata) {
+            if(!err) {
+                console.log(metadata);
+            }
+        });
+    });
 });
